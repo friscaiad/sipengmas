d51b2e87e7df00e0a5995802205d3e633db909ae
miridium-electrode <checkerdance@yahoo.com>
2022-05-25 12:21:28 +0700

    add email and password as loginable

--- database/migrations/2022_04_11_155913_pegawai.php
+++ database/migrations/2022_04_11_155913_pegawai.php
@@ -16,6 +16,10 @@ class Pegawai extends Migration
         Schema::create('pegawai', function (Blueprint $table) {
             $table->string('nip', 50);
             $table->string('nama_lengkap', 255);
+            $table->string('email', 255)->unique();
+            $table->string('password', 255);
+            $table->rememberToken();
+            $table->timestamps();
             $table->primary('nip');
             $table->foreignId('role_id')->constrained('roles');
         });
