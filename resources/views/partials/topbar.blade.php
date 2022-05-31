bdb3ba18a153a03cafb8f54832c57f8c77ea41f7
alexdoffgit <noreply@plea.se>
2022-05-31 15:13:37 +0700

    no color navbar

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -1,26 +1,26 @@
 <header>
-    <nav class="navbar navbar-expand-sm sticky-top bg-primary" style="position: fixed; width: 100%; z-index: 100 margin-left: 3px; margin-right: 3px">
+    <nav class="navbar navbar-expand-sm sticky-top" style="position: fixed; width: 100%; z-index: 100 margin-left: 3px; margin-right: 3px">
         <div class="container-fluid">
-            <a href="#" class="navbar-brand text-white">
+            <a href="#" class="navbar-brand">
                 <img src="{{ asset('images/siabdilogo.png') }}" alt="SIABDI">
                 </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="bi bi-list text-white"></i>
+                <i class="bi bi-list"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav navbar-kanan">
                     <li class="nav-item">
-                        <a href="#" class="nav-link text-white">Home</a>
+                        <a href="#" class="nav-link">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link text-nowrap text-white">Data Pengmas</a>
+                        <a href="#" class="nav-link text-nowrap">Data Pengmas</a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link text-nowrap text-white">Contact Us</a>
+                        <a href="#" class="nav-link text-nowrap">Contact Us</a>
                     </li>
                     <li class="nav-item dropdown no-arrow">
-                        <a class="nav-link dropdown-toggle text-white" href="#" id="userDropdown" role="button"
+                        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="bi bi-person-circle"></i>
                         </a>
