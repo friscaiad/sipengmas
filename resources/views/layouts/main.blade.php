3dee4c614c8ab106cb7a3b760b0bc405c8342b5f
alexdoffgit <noreply@plea.se>
2022-06-02 05:01:45 +0700

    use compiled import

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,16 +11,11 @@
 
     <title>SIABDI - {{ $title }}</title>
 
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-    <link href="css/stylesheet.css" rel="stylesheet">
-    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="{{ asset('css/stylesheet.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/bs.css')}} " rel="stylesheet">
 
 </head>
 
@@ -53,15 +48,9 @@
 
 
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-    <script src="js/sb-admin-2.min.js"></script>
-    <script src="vendor/chart.js/Chart.min.js"></script>
-    <script src="js/demo/chart-area-demo.js"></script>
-    <script src="js/demo/chart-pie-demo.js"></script>
-    <script src="js/bootstrap.js"></script>
-
+    
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/bs.js') }}"></script>
 </body>
 
 </html>
